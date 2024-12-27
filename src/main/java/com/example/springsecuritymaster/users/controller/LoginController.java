3d2797e8e77ac8213880d152cb62887fe31a1b0a
jeus1998 <zeus20240228@gmail.com>
2024-12-27 19:02:48 +0900

    [2024:12:27] Custom 로그아웃

--- src/main/java/com/example/springsecuritymaster/users/controller/LoginController.java
+++ src/main/java/com/example/springsecuritymaster/users/controller/LoginController.java
@@ -1,5 +1,11 @@
 package com.example.springsecuritymaster.users.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,5 +18,13 @@ public class LoginController {
     @GetMapping("/signup")
     public String signup(){
         return "/login/signup";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if(authentication != null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/login";
     }
 }
