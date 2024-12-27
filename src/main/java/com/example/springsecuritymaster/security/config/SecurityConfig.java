9bbac634da019c2b924104bf607ea1ae65620fa7
jeus1998 <zeus20240228@gmail.com>
2024-12-28 03:18:35 +0900

    [2024:12:28] Rest 인증 필터 구현 - RestAuthenticationFilter

--- src/main/java/com/example/springsecuritymaster/security/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritymaster/security/config/SecurityConfig.java
@@ -1,18 +1,22 @@
 package com.example.springsecuritymaster.security.config;
 
+import com.example.springsecuritymaster.security.filters.RestAuthenticationFilter;
 import com.example.springsecuritymaster.security.handler.FormAccessDeniedHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -25,17 +29,29 @@ public class SecurityConfig {
     @Bean
     @Order(1)
     public SecurityFilterChain restSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
         http
                 .securityMatcher("/api/**")
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/images/**",
                                 "/js/**", "/favicon.*", "/*/icon/-*").permitAll()
                         .anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable);
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(
+                        restAuthenticationFilter(authenticationManager),
+                        UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
+    private RestAuthenticationFilter restAuthenticationFilter(AuthenticationManager authenticationManager) {
+        RestAuthenticationFilter restAuthenticationFilter = new RestAuthenticationFilter();
+        restAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        return restAuthenticationFilter;
+    }
     @Bean
     @Order(2)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
---
+++ src/main/java/com/example/springsecuritymaster/security/filters/RestAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package com.example.springsecuritymaster.security.filters;
+
+import com.example.springsecuritymaster.domain.dto.AccountDto;
+import com.example.springsecuritymaster.security.token.RestAuthenticationToken;
+import com.example.springsecuritymaster.util.WebUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+
+public class RestAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    public RestAuthenticationFilter() {
+        super(new AntPathRequestMatcher("/api/login", "POST"));
+    }
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+
+        if(!HttpMethod.POST.matches(request.getMethod()) || WebUtil.isAjax(request)){
+            throw new IllegalArgumentException("Authentication method not supported");
+        }
+
+        AccountDto accountDto = objectMapper.readValue(request.getReader(), AccountDto.class);
+        if(!StringUtils.hasText(accountDto.getUsername()) || StringUtils.hasText(accountDto.getPassword())){
+            throw new AuthenticationServiceException("Username or Password is not provided");
+        }
+
+        RestAuthenticationToken authenticationToken = new RestAuthenticationToken(accountDto.getUsername(), accountDto.getPassword());
+        return getAuthenticationManager().authenticate(authenticationToken);
+    }
+}
