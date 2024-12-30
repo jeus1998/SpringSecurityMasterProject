f46a37a557688e000e283a3eaa18f5cc69c2ab95
jeus1998 <zeus20240228@gmail.com>
2024-12-30 15:15:54 +0900

    [2024:12:30] Rest 인증 상태 영속화 - SecurityContextRepository

--- src/main/java/com/example/springsecuritymaster/security/config/SecurityConfig.java
+++ src/main/java/com/example/springsecuritymaster/security/config/SecurityConfig.java
@@ -45,14 +45,14 @@ public class SecurityConfig {
                         .anyRequest().permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
                 .addFilterBefore(
-                        restAuthenticationFilter(authenticationManager),
+                        restAuthenticationFilter(http, authenticationManager),
                         UsernamePasswordAuthenticationFilter.class)
                 .authenticationManager(authenticationManager);
 
         return http.build();
     }
-    private RestAuthenticationFilter restAuthenticationFilter(AuthenticationManager authenticationManager) {
-        RestAuthenticationFilter restAuthenticationFilter = new RestAuthenticationFilter();
+    private RestAuthenticationFilter restAuthenticationFilter(HttpSecurity http, AuthenticationManager authenticationManager) {
+        RestAuthenticationFilter restAuthenticationFilter = new RestAuthenticationFilter(http);
         restAuthenticationFilter.setAuthenticationManager(authenticationManager);
         restAuthenticationFilter.setAuthenticationSuccessHandler(restSuccessHandler);
         restAuthenticationFilter.setAuthenticationFailureHandler(restFailureHandler);
--- src/main/java/com/example/springsecuritymaster/security/filters/RestAuthenticationFilter.java
+++ src/main/java/com/example/springsecuritymaster/security/filters/RestAuthenticationFilter.java
@@ -9,18 +9,31 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.context.DelegatingSecurityContextRepository;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.util.StringUtils;
-
 import java.io.IOException;
 
 public class RestAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
     private final ObjectMapper objectMapper = new ObjectMapper();
-    public RestAuthenticationFilter() {
+    public RestAuthenticationFilter(HttpSecurity http) {
         super(new AntPathRequestMatcher("/api/login", "POST"));
+        setSecurityContextRepository(getSecurityContextRepository(http));
+    }
+    private SecurityContextRepository getSecurityContextRepository(HttpSecurity http) {
+        SecurityContextRepository securityContextRepository = http.getSharedObject(SecurityContextRepository.class);
+        if(securityContextRepository == null || securityContextRepository instanceof RequestAttributeSecurityContextRepository){
+           return new DelegatingSecurityContextRepository(
+                    new RequestAttributeSecurityContextRepository(), new HttpSessionSecurityContextRepository());
+        }
+        return securityContextRepository;
     }
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
